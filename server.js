402ec8528311636b9b39dcb28789f68d76033b91
anshukushwaha07 <anshu.kush007@gmail.com>
2024-12-26 21:31:09 +0530

    server files changes

--- server.js
+++ server.js
@@ -9,5 +9,17 @@ const dbConnection = require("./db");
 app.use("/api/cars/", require("./routes/carsRoute"));
 app.use("/api/users/", require("./routes/usersRoute"));
 app.use("/api/bookings/", require("./routes/bookingsRoute"));
-app.get("/", (req, res) => res.send("Hello World"));
-app.listen(port, () => console.log(`Node JS server started in port ${port}`));
+
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`));
